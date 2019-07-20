9a6b1638b041fa8bb0dbf82eb092c3c90e219538
xyqai <V_xiaqiang@saicmotor.com>
2019-07-20 17:14:08 +0800

    初始化

---
+++ miniprogram/pages/game/gameRules/gameRules.js
@@ -0,0 +1,56 @@
+// pages/game/gameRules/gameRules.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  goStart() {
+    wx.navigateTo({
+      url: '../startGame/startGame',
+    })
+  },
+  goBackIndex() {
+    wx.navigateBack({
+      delate: 1
+    })
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
